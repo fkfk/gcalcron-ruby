b690e516c03457ef68075d51a96c6df27f3ea744
Koutarou Tanaka <from.kyushu.island@gmail.com>
2009-05-17 16:00:07 +0900

    change @list instance val in GCalCron::Cron

--- lib/gcalcron/cron.rb
+++ lib/gcalcron/cron.rb
@@ -2,7 +2,7 @@ class GCalCron
   class Cron
     def initialize user=nil
       @crontab = CronEdit::Crontab.new user
-      @list = @crontab.list.map {|k,v| v}
+      @list = @crontab.list.map {|k,v| CronEdit::CronEntry.new(v).to_hash}
     end
     attr_reader :list
   end
